0eb951a5a6ed8e15458c23f4776e066c38b6a0ce
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-11 10:46:31 -0500

    refactor(DemoApplication): add DemoApplication to infrastructure

--- src/main/java/com/Test/demo/infrastructure/DemoApplication.java
+++ src/main/java/com/Test/demo/infrastructure/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.Test.demo;
+package com.Test.demo.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
