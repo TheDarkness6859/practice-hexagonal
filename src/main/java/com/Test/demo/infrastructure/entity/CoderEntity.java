2f6c57aaef7cfceea108691b0ad364be5b8baa66
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-12 07:29:11 -0500

    refactor(CoderEntity): add JPA and hibernate to persistance in database

--- src/main/java/com/Test/demo/infrastructure/entity/CoderEntity.java
+++ src/main/java/com/Test/demo/infrastructure/entity/CoderEntity.java
@@ -1,16 +1,29 @@
 package com.Test.demo.infrastructure.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.annotation.Generated;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.UUID;
+
+@Entity
+@Table(name = "coders")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CoderEntity {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String clan;
 
 }
