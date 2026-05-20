f24fc05492336823dbb3ba324c6aacf0e5f929fb
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-20 06:53:37 -0500

    refactor(Coder): clean domain whithout annotations of springboot

--- src/main/java/com/Test/demo/domain/Coder.java
+++ src/main/java/com/Test/demo/domain/Coder.java
@@ -1,18 +1,44 @@
 package com.Test.demo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Coder {
 
     private UUID id;
     private String name;
     private String clan;
+
+    public Coder(UUID id, String name, String clan) {
+
+        this.id = id;
+        this.name = name;
+        this.clan = clan;
+
+    }
+
+    public String getClan() {
+        return clan;
+    }
+
+    public void setClan(String clan) {
+        this.clan = clan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
 }
