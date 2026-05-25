fdb35bf9623e23413726c485f1f4ea55df201b04
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-25 08:52:44 -0500

    feat(CategoryEntity): add entity with relation Many to Many

---
+++ src/main/java/com/Test/demo/infrastructure/entity/CategoryEntity.java
@@ -0,0 +1,31 @@
+package com.Test.demo.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<CoderEntity> coders = new HashSet<>();
+
+}
