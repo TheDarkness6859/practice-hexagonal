1c2c0fa39bf455e3ce730837c0082285b770fb9d
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-29 11:17:51 -0500

    refactor(DatabaseRepo): make interface generic

--- src/main/java/com/Test/demo/infrastructure/adapter/persistance/DatabaseRepo.java
+++ src/main/java/com/Test/demo/infrastructure/adapter/persistance/DatabaseRepo.java
@@ -2,9 +2,11 @@ package com.Test.demo.infrastructure.adapter.persistance;
 
 import com.Test.demo.infrastructure.entity.CoderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.UUID;
 
-public interface DatabaseRepo extends JpaRepository<CoderEntity, UUID> {
+@NoRepositoryBean
+public interface DatabaseRepo<T> extends JpaRepository<T, UUID> {
 
 }
