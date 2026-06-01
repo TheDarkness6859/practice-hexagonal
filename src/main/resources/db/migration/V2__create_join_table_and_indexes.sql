078affd1c37d8b4632761d52a9c77f8c725604d3
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-01 07:06:16 -0500

    typo(migration): remove typos in database versions

--- src/main/resources/db/migration/V2__create_join_table_and_indexes.sql
+++ src/main/resources/db/migration/V2__create_join_table_and_indexes.sql
@@ -5,10 +5,10 @@ CREATE TABLE coders_categories (
                                    categoria_id UUID NOT NULL,
                                    PRIMARY KEY (coder_id, categoria_id),
                                    CONSTRAINT fk_cc_coder FOREIGN KEY (coder_id) REFERENCES coders(id) ON DELETE CASCADE,
-                                   CONSTRAINT fk_cc_categoria FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE
+                                   CONSTRAINT fk_cc_categoria FOREIGN KEY (categoria_id) REFERENCES categories(id) ON DELETE CASCADE
 );
 
 -- index to improve the performance in supabase
 CREATE INDEX idx_coders_clan ON coders(clan_id);
-CREATE INDEX idx_coders_active ON coders(activo);
-CREATE INDEX idx_clanes_names ON clanes(nombre);
+CREATE INDEX idx_coders_active ON coders(active);
+CREATE INDEX idx_clans_names ON clans(name);
--- src/main/resources/db/migration/V3__seed_initial_data.sql
+++ src/main/resources/db/migration/V3__seed_initial_data.sql
@@ -1,29 +1,29 @@
 -- V3: seed of data
 
 -- 1. Insertar Clanes
-INSERT INTO clanes (nombre, descripcion) VALUES
+INSERT INTO clans (name, description) VALUES
                                              ('Meta-Galactics', 'Clan enfocado en backend avanzado y arquitectura cloud'),
                                              ('Cyber-Coders', 'Especialistas en desarrollo frontend interactivo y UX');
 
 -- 2. Insert categories (Languages)
-INSERT INTO categories(nombre) VALUES
+INSERT INTO categories(name) VALUES
                                     ('Java'),
                                     ('TypeScript'),
                                     ('PostgreSQL'),
                                     ('Python');
 
 -- 3. Insert coders with his Id to the Clans.
-INSERT INTO coders (name, activo, clan_id) VALUES
-                                               ('Emmanuel Garcia', true, (SELECT id FROM clanes WHERE nombre = 'Meta-Galactics')),
-                                               ('Steven Developer', true, (SELECT id FROM clanes WHERE nombre = 'Meta-Galactics')),
-                                               ('Yolanda Code', true, (SELECT id FROM clanes WHERE nombre = 'Cyber-Coders')),
-                                               ('Sebastian Dev', true, (SELECT id FROM clanes WHERE nombre = 'Cyber-Coders'));
+INSERT INTO coders (name, active, clan_id) VALUES
+                                               ('Emmanuel Garcia', true, (SELECT id FROM clans WHERE name = 'Meta-Galactics')),
+                                               ('Steven Developer', true, (SELECT id FROM clans WHERE name = 'Meta-Galactics')),
+                                               ('Yolanda Code', true, (SELECT id FROM clans WHERE name = 'Cyber-Coders')),
+                                               ('Sebastian Dev', true, (SELECT id FROM clans WHERE name = 'Cyber-Coders'));
 
 -- 4. Middle table (Many to Many)
 INSERT INTO coders_categories (coder_id, categoria_id) VALUES
-                                                           ((SELECT id FROM coders WHERE name = 'Emmanuel Garcia'), (SELECT id FROM categorias WHERE nombre = 'Java')),
-                                                           ((SELECT id FROM coders WHERE name = 'Emmanuel Garcia'), (SELECT id FROM categorias WHERE nombre = 'PostgreSQL')),
-                                                           ((SELECT id FROM coders WHERE name = 'Steven Developer'), (SELECT id FROM categorias WHERE nombre = 'Java')),
-                                                           ((SELECT id FROM coders WHERE name = 'Yolanda Code'), (SELECT id FROM categorias WHERE nombre = 'TypeScript')),
-                                                           ((SELECT id FROM coders WHERE name = 'Sebastian Dev'), (SELECT id FROM categorias WHERE nombre = 'TypeScript')),
-                                                           ((SELECT id FROM coders WHERE name = 'Sebastian Dev'), (SELECT id FROM categorias WHERE nombre = 'Python'));
+                                                           ((SELECT id FROM coders WHERE name = 'Emmanuel Garcia'), (SELECT id FROM categories WHERE name = 'Java')),
+                                                           ((SELECT id FROM coders WHERE name = 'Emmanuel Garcia'), (SELECT id FROM categories WHERE name = 'PostgreSQL')),
+                                                           ((SELECT id FROM coders WHERE name = 'Steven Developer'), (SELECT id FROM categories WHERE name = 'Java')),
+                                                           ((SELECT id FROM coders WHERE name = 'Yolanda Code'), (SELECT id FROM categories WHERE name = 'TypeScript')),
+                                                           ((SELECT id FROM coders WHERE name = 'Sebastian Dev'), (SELECT id FROM categories WHERE name = 'TypeScript')),
+                                                           ((SELECT id FROM coders WHERE name = 'Sebastian Dev'), (SELECT id FROM categories WHERE name = 'Python'));
